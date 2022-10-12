d633ca229c65c4355e995bd447cad96f74cf4e36
Denna Mandela <denna.mandela13@gmail.com>
2022-10-13 01:18:08 +0700

    movie app

--- script.js
+++ script.js
@@ -1,29 +1,84 @@
-// mendeklarasikan variabel dan mengambil element dari html
-const API_URL_MOVIE = "https://api.themoviedb.org/3/discover/movie?api_key=8a7b3c0bea4f8ac64aaf7351a83e204a&sort_by=popularity.desc"
+// mendeklarasikan variabel dan mengambil api movie
+const listMovieAPI = "https://api.themoviedb.org/3/discover/movie?api_key=8a7b3c0bea4f8ac64aaf7351a83e204a&sort_by=popularity.desc"
+const IMG_PATH = "https://image.tmdb.org/t/p/w500/";
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?api_key=8a7b3c0bea4f8ac64aaf7351a83e204a&query=&page=1";
+const column = document.querySelector(".col");
 
-const cari = document.getElementById("search");
+// list movie berdasarkan popularity
+getDataMovie = async () => {
+    let response = await fetch(listMovieAPI);
+    let movie = await response.json();
 
+    let listMovie = ' ';
+
+   movie.results.forEach(item => {
+       let list = ` <div class="card shadow-sm">
+            <img src="${IMG_PATH + item.poster_path}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card-text">${item.title}</p>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="btn-group">
+                            <p class="card-text">${item.release_date}</p>
+                        </div>
+                        <small class="text-muted">${item.vote_average}</small>
+                    </div>
+                </div>
+            </div>`;
+        listMovie += list;
+   });
+
+   column.innerHTML = listMovie;
+}
+getDataMovie();
+
+// fitur search
 const form = document.getElementById("myForm");
+const search = document.getElementById("search");
 
-const tampil = document.getElementById("content");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-// mengambil data api movie
-getMovie(API_URL_MOVIE);
+    const searchTerm = search.value;
+    if(searchTerm) {
+        getDataMovie(SEARCH_API+'&query='+searchTerm)
+    }else{
+        getDataMovie();
+    }
+});
 
-async function getMovie(url) {
-    let response = await fetch(url);
-    let responseData = await response.json();
-    showlistMovie(responseData);
-}
+// async function getDataMovie() {
+//     try {
+//         let response = await fetch(listMovieAPI);
+//         return await response.json();
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
 
-function showlistMovie(movies) {
+// async function getMovie() {
+//     let movies = await getDataMovie();
+//     movies.results.forEach(item => {
+//         listMovie.innerHTML += `<img src="${IMG_PATH + item.poster_path}" class="card-img-top" alt="...">
+//          <div class="card-body">
+//             <p class="card-text">${item.title}</p>
+//             <div class="d-flex justify-content-between align-items-center">
+//                 <div class="btn-group">
+//                   <p class="card-text">${item.release_date}</p>
+//                 </div>
+//                 <small class="text-muted">${item.vote_average}</small>
+//             </div>
+//         </div>`;
+        
+//     });
+// }
 
-    movies.forEach(itemMovie => {
-        console.log(itemMovie);
-    });
+// getMovie();
+
+    
 
 
 
-}
+
+    
 
 
